7a03c1449e71648750b55db6c24dbe330e16f96f
Kevin Balke <fughilli@gmail.com>
2020-09-20 00:08:28 -0700

    Made DEBUG log noop when not explicitly enabled

--- util/logging.h
+++ util/logging.h
@@ -69,6 +69,7 @@ class _Logger {
 };
 
 class _DummyLogger {
+ public:
   template <typename T>
   _DummyLogger& operator<<(const T& value) {
     return *this;
@@ -76,9 +77,22 @@ class _DummyLogger {
 };
 
 #if defined(DISABLE_LOGGING)
+// Logging is disabled. Redirect all log calls to the dummy logger.
 #define LOG(level) _DummyLogger()
 #else
-#define LOG(level) _Logger(level, __FILE__, __LINE__)
+#define __LOG_FUNCTION_COMMON(level) _Logger(level, __FILE__, __LINE__)
+
+#if defined(ENABLE_DEBUG_LOGGING)
+#define __LOG_FUNCTION_DEBUG(level) __LOG_FUNCTION_COMMON(level)
+#else
+#define __LOG_FUNCTION_DEBUG(level) _DummyLogger()
+#endif
+
+#define __LOG_FUNCTION_INFO(level) __LOG_FUNCTION_COMMON(level)
+#define __LOG_FUNCTION_WARNING(level) __LOG_FUNCTION_COMMON(level)
+#define __LOG_FUNCTION_ERROR(level) __LOG_FUNCTION_COMMON(level)
+
+#define LOG(level) (__LOG_FUNCTION_##level(level))
 #endif
 
 #endif  // UTIL_LOGGING_H_
