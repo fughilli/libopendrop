8b230ac5c7400d74cfe60f99614b18c3486c3229
Kevin Balke <fughilli@gmail.com>
2020-09-29 00:43:33 -0700

    Implemented CHECK macro and FATAL loglevel

--- util/logging.h
+++ util/logging.h
@@ -1,6 +1,7 @@
 #ifndef UTIL_LOGGING_H_
 #define UTIL_LOGGING_H_
 
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
 
@@ -10,23 +11,29 @@ constexpr static bool kDebugLoggingEnabled = true;
 constexpr static bool kDebugLoggingEnabled = false;
 #endif
 
-enum _LogLevel { DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3 };
+enum _LogLevel { DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3, FATAL = 4 };
 
 class _Logger {
  public:
   _Logger(_LogLevel level, const char* file, int line)
-      : level_(level), file_(file), line_(line) {}
+      : level_(level), file_(file), line_(line), condition_(true) {}
   template <typename T>
   _Logger& operator<<(const T& value) {
-    if (!kDebugLoggingEnabled && level_ == DEBUG) {
+    if (!Enabled()) {
       return *this;
     }
     logline_stream_ << value;
     return *this;
   }
 
+  // Sets the value of the condition for this logger instance.
+  _Logger& SetCondition(bool condition) {
+    condition_ = condition;
+    return *this;
+  }
+
   ~_Logger() {
-    if (!kDebugLoggingEnabled && level_ == DEBUG) {
+    if (!Enabled()) {
       return;
     }
 
@@ -38,12 +45,27 @@ class _Logger {
         break;
       case WARNING:
       case ERROR:
+      case FATAL:
         WriteHeader(std::cerr);
         std::cerr << logline_stream_.str() << std::endl;
+    }
+
+    if (level_ == FATAL) {
+      abort();
     }
   }
 
  private:
+  bool Enabled() const {
+    if (!kDebugLoggingEnabled && level_ == DEBUG) {
+      // If this is a debug log, and debug logging is disabled, don't log.
+      return false;
+    }
+
+    // Otherwise return the condition.
+    return condition_;
+  }
+
   void WriteHeader(std::ostream& ostream) {
     switch (level_) {
       case DEBUG:
@@ -58,6 +80,9 @@ class _Logger {
       case ERROR:
         ostream << "[ERROR]";
         break;
+      case FATAL:
+        ostream << "[FATAL]";
+        break;
     }
     ostream << "[" << file_ << ":" << line_ << "]  ";
   }
@@ -66,6 +91,7 @@ class _Logger {
   const char* file_;
   int line_;
   std::stringstream logline_stream_;
+  bool condition_;
 };
 
 class _DummyLogger {
@@ -91,8 +117,15 @@ class _DummyLogger {
 #define __LOG_FUNCTION_INFO(level) __LOG_FUNCTION_COMMON(level)
 #define __LOG_FUNCTION_WARNING(level) __LOG_FUNCTION_COMMON(level)
 #define __LOG_FUNCTION_ERROR(level) __LOG_FUNCTION_COMMON(level)
+#define __LOG_FUNCTION_FATAL(level) __LOG_FUNCTION_COMMON(level)
 
 #define LOG(level) (__LOG_FUNCTION_##level(level))
 #endif
+
+#define CHECK_NULL(pointer)                      \
+  (LOG(FATAL).SetCondition((pointer) == nullptr) \
+   << "CHECK_NULL FAIL ((" #pointer ") == nullptr): ")
+#define CHECK(expression) \
+  (LOG(FATAL).SetCondition(!(expression)) << "CHECK FAIL (" #expression "): ")
 
 #endif  // UTIL_LOGGING_H_
