118d82a0af4a395acb3e76c4317b3955dda21d65
Kevin Balke <fughilli@gmail.com>
2020-09-29 02:43:08 -0700

    Implemented Alien Rorschach preset

---
+++ preset/alien_rorschach/alien_rorschach.h
@@ -0,0 +1,29 @@
+#ifndef PRESETS_ALIEN_RORSCHACH_ALIEN_RORSCHACH_H_
+#define PRESETS_ALIEN_RORSCHACH_ALIEN_RORSCHACH_H_
+
+#include "libopendrop/gl_interface.h"
+#include "libopendrop/gl_render_target.h"
+#include "libopendrop/preset/preset.h"
+
+namespace opendrop {
+
+class AlienRorschach : public Preset {
+ public:
+  AlienRorschach(int width, int height);
+
+ protected:
+  void OnDrawFrame(
+      absl::Span<const float> samples, std::shared_ptr<GlobalState> state,
+      std::shared_ptr<gl::GlRenderTarget> output_render_target) override;
+  void OnUpdateGeometry() override;
+
+ private:
+  std::shared_ptr<gl::GlRenderTarget> front_render_target_;
+  std::shared_ptr<gl::GlRenderTarget> back_render_target_;
+  std::shared_ptr<gl::GlProgram> warp_program_;
+  std::shared_ptr<gl::GlProgram> composite_program_;
+};
+
+}  // namespace opendrop
+
+#endif  // PRESETS_ALIEN_RORSCHACH_ALIEN_RORSCHACH_H_
---
+++ preset/alien_rorschach/composite.fsh
@@ -0,0 +1,29 @@
+#version 120
+
+uniform sampler2D render_target;
+uniform ivec2 render_target_size;
+uniform float power;
+uniform float energy;
+
+varying vec2 screen_uv;
+
+vec2 rotate(vec2 vec, float theta) {
+  float s = sin(theta);
+  float c = cos(theta);
+
+  return vec2(vec.x * c - vec.y * s, vec.x * s + vec.y * c);
+}
+
+vec2 screen_to_tex(vec2 screen_uv) { return (screen_uv + vec2(1., 1.)) * 0.5; }
+
+void main() {
+  vec2 screen_uv_p = rotate(screen_uv, sin(energy * 20 + power) / 10);
+  screen_uv_p.x = abs(screen_uv_p.x);
+
+  vec2 tex_uv = screen_to_tex(screen_uv_p);
+  vec2 offset = (3. + power * 10) / render_target_size;
+  gl_FragColor = texture2D(render_target, tex_uv) * 1. -
+                 (texture2D(render_target, tex_uv + vec2(offset.x, 0.)) +
+                  texture2D(render_target, tex_uv + vec2(0., offset.y))) *
+                     0.2;
+}
