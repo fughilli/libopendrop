c3cc7aeadf18bd5edf0ca5f20236186f1bc08acf
Kevin Balke <fughilli@gmail.com>
2020-11-02 23:28:46 -0800

    Correct implementation of HSV->RGB

--- util/colors.cc
+++ util/colors.cc
@@ -1,17 +1,35 @@
 #include "libopendrop/util/colors.h"
 
 #define _USE_MATH_DEFINES
+#include <algorithm>
 #include <cmath>
 
 namespace opendrop {
 
 glm::vec3 HsvToRgb(glm::vec3 hsv) {
-  auto normalized_offset_sin = [&](float x, float offset) {
-    return (1.0f + sin((x + offset) * M_PI * 2)) / 2;
-  };
-  return glm::vec3(normalized_offset_sin(hsv.x, 0.0f),
-                   normalized_offset_sin(hsv.x, 0.333f),
-                   normalized_offset_sin(hsv.x, 0.666f));
+  // C = V x Sv
+  float c = std::clamp(hsv.y, 0.0f, 1.0f) * std::clamp(hsv.z, 0.0f, 1.0f);
+
+  // H' = H/60deg
+  // X = chroma x (1 - |H' mod 2 - 1|)
+  float x = c * (1.0f - std::abs(std::fmod(hsv.x * 6.0f, 2.0f) - 1.0f));
+
+  switch (static_cast<int64_t>(std::floor(hsv.x * 6)) % 6) {
+    case 0:
+      return glm::vec3(c, x, 0.0f);
+    case 1:
+      return glm::vec3(x, c, 0.0f);
+    case 2:
+      return glm::vec3(0.0f, c, x);
+    case 3:
+      return glm::vec3(0.0f, x, c);
+    case 4:
+      return glm::vec3(x, 0.0f, c);
+    case 5:
+      return glm::vec3(c, 0.0f, x);
+    default:
+      return glm::vec3(0.0f, 0.0f, 0.0f);
+  }
 }
 
 }  // namespace opendrop
---
+++ util/colors_test.cc
@@ -0,0 +1,33 @@
+#include "libopendrop/util/colors.h"
+
+#include <glm/geometric.hpp>
+
+#include "googletest/include/gtest/gtest.h"
+
+namespace opendrop {
+namespace {
+
+constexpr float kEpsilon = 1e-6f;
+
+struct TestValues {
+  glm::vec3 hsv_input;
+  glm::vec3 rgb_expected_output;
+};
+
+class ColorsParameterizedTest : public ::testing::TestWithParam<TestValues> {};
+TEST_P(ColorsParameterizedTest, HsvToRgbProducesCorrectValues) {
+  TestValues values = GetParam();
+
+  auto rgb = HsvToRgb(values.hsv_input);
+  EXPECT_LE(glm::length(rgb - values.rgb_expected_output), kEpsilon);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CorrectValuesSequence, ColorsParameterizedTest,
+    ::testing::Values(TestValues{{0, 1, 1}, {1, 0, 0}},
+                      TestValues{{1.0f / 3.0f, 1, 1}, {0, 1, 0}},
+                      TestValues{{2.0f / 3.0f, 1, 1}, {0, 0, 1}},
+                      TestValues{{0, 0, 0}, {0, 0, 0}}));
+
+}  // namespace
+}  // namespace opendrop
