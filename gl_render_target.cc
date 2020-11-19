1892939faf2ccfaa8a8daaf2408afd700edc09e0
Kevin Balke <fughilli@gmail.com>
2020-11-18 19:33:53 -0800

    Fix depth/stencil attachment initialization and improve debug messages.
    This change fixes a bug where GlRenderTarget would attempt to deallocate
    the depth/stencil attachment even if it was not allocated, and another
    issue where the components of the render target are deallocated in the
    wrong order.
    
    Additionally, more verbose logging is added for issues arising from
    framebuffer completion.

--- gl_render_target.cc
+++ gl_render_target.cc
@@ -32,11 +32,38 @@ GlRenderTargetActivation::GlRenderTargetActivation(
              << render_target_->texture_handle() << " for framebuffer "
              << render_target_->framebuffer_handle() << " and renderbuffer "
              << render_target_->renderbuffer_handle();
-  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-    LOG(ERROR) << "Failed to configure framebuffer";
-    return;
-  } else {
+  auto framebuffer_status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+  if (framebuffer_status == GL_FRAMEBUFFER_COMPLETE) {
     LOG(DEBUG) << "Successfully configured framebuffer";
+  } else {
+    LOG(ERROR) << "Failed to configure framebuffer";
+    switch (framebuffer_status) {
+      case GL_FRAMEBUFFER_UNDEFINED:
+        LOG(ERROR) << "GL_FRAMEBUFFER_UNDEFINED";
+        break;
+      case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+        LOG(ERROR) << "GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT";
+        break;
+      case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+        LOG(ERROR) << "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT";
+        break;
+      case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+        LOG(ERROR) << "GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER";
+        break;
+      case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+        LOG(ERROR) << "GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER";
+        break;
+      case GL_FRAMEBUFFER_UNSUPPORTED:
+        LOG(ERROR) << "GL_FRAMEBUFFER_UNSUPPORTED";
+        break;
+      case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+        LOG(ERROR) << "GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE";
+        break;
+      case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
+        LOG(ERROR) << "GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS";
+        break;
+    }
+    return;
   }
 
   glViewport(0, 0, render_target_->width(), render_target_->height());
@@ -45,8 +72,8 @@ GlRenderTargetActivation::GlRenderTargetActivation(
 }
 
 GlRenderTargetActivation::~GlRenderTargetActivation() {
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
   glBindRenderbuffer(GL_RENDERBUFFER, 0);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
   LOG(DEBUG) << "Unbound framebuffer";
 }
 
@@ -84,13 +111,14 @@ absl::StatusOr<std::shared_ptr<GlRenderTarget>> GlRenderTarget::MakeShared(
 
 GlRenderTarget::~GlRenderTarget() {
   LOG(DEBUG) << "Disposing render target";
-  texture_manager_->Deallocate(texture_unit_);
-  glDeleteTextures(1, &texture_handle_);
-  glDeleteFramebuffers(1, &framebuffer_handle_);
-
   if (options_.enable_depth) {
     glDeleteTextures(1, &depth_buffer_handle_);
   }
+
+  texture_manager_->Deallocate(texture_unit_);
+  glDeleteTextures(1, &texture_handle_);
+  glDeleteFramebuffers(1, &framebuffer_handle_);
+  glDeleteFramebuffers(1, &renderbuffer_handle_);
 }
 
 void GlRenderTarget::UpdateGeometry(int width, int height) {
@@ -109,10 +137,14 @@ void GlRenderTarget::UpdateGeometry(int width, int height) {
                GL_UNSIGNED_BYTE, 0);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glBindTexture(GL_TEXTURE_2D, depth_buffer_handle_);
-  LOG(DEBUG) << "Bound depth texture: " << depth_buffer_handle_;
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, width_, height_, 0,
-               GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, 0);
+  if (options_.enable_depth) {
+    glBindTexture(GL_TEXTURE_2D, depth_buffer_handle_);
+    LOG(DEBUG) << "Bound depth texture: " << depth_buffer_handle_;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, width_, height_, 0,
+                 GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  }
   glBindTexture(GL_TEXTURE_2D, 0);
   LOG(DEBUG) << "Unbound texture";
 }
