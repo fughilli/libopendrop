a7a9c1621c18192e4e6a177a58b53b6f75a4bf88
Kevin Balke <fughilli@gmail.com>
2020-12-03 02:26:18 -0800

    Added LogLinear function

--- util/math.h
+++ util/math.h
@@ -3,6 +3,8 @@
 
 #include <cmath>
 
+#include "libopendrop/util/logging.h"
+
 namespace opendrop {
 
 constexpr float kEpsilon = 1e-6f;
@@ -18,6 +20,20 @@ inline T SafeDivide(T a, T b) {
     return a;
   }
   return a / b;
+}
+
+template <typename T>
+T LogLinear(T arg, T n) {
+  // TODO: This implementation should find some more interesting analytic
+  // interpolation between linear and logarithmic such that the curve does not
+  // asymptotically approach the log regime as arg=>0, and the linear regime as
+  // arg=>inf.
+  //
+  // Perhaps an answer lies at
+  // <https://math.stackexchange.com/questions/107245/continuum-between-linear-and-logarithmic>?
+  CHECK(arg > 0) << "arg must be positive.";
+  CHECK(n <= 1 && n >= 0) << "n must be between 0 and 1, inclusive.";
+  return n * arg + (1 - n) * std::log(arg);
 }
 
 }  // namespace opendrop
--- util/math_test.cc
+++ util/math_test.cc
@@ -6,23 +6,38 @@ namespace opendrop {
 namespace {
 
 TEST(MathTest, AlmostEqualTrueForSameValue) {
-  EXPECT_TRUE(AlmostEqual<int>(0,0));
-  EXPECT_TRUE(AlmostEqual<float>(0,0));
-  EXPECT_TRUE(AlmostEqual<double>(11.123,11.123));
-  EXPECT_TRUE(AlmostEqual<double>(5,5));
-  EXPECT_TRUE(AlmostEqual<double>(-22.345,-22.345));
+  EXPECT_TRUE(AlmostEqual<int>(0, 0));
+  EXPECT_TRUE(AlmostEqual<float>(0, 0));
+  EXPECT_TRUE(AlmostEqual<double>(11.123, 11.123));
+  EXPECT_TRUE(AlmostEqual<double>(5, 5));
+  EXPECT_TRUE(AlmostEqual<double>(-22.345, -22.345));
 }
 
 TEST(MathTest, AlmostEqualTrueForSlightlyDifferentValues) {
-  EXPECT_TRUE(AlmostEqual(1.0f,1.0f + 1e-10f));
-  EXPECT_TRUE(AlmostEqual(1.0f,1.0f + 1e-8f));
-  EXPECT_TRUE(AlmostEqual(1.0f,1.0f + 1e-6f));
-  EXPECT_FALSE(AlmostEqual(1.0f,1.0f + 1e-5f));
+  EXPECT_TRUE(AlmostEqual(1.0f, 1.0f + 1e-10f));
+  EXPECT_TRUE(AlmostEqual(1.0f, 1.0f + 1e-8f));
+  EXPECT_TRUE(AlmostEqual(1.0f, 1.0f + 1e-6f));
+  EXPECT_FALSE(AlmostEqual(1.0f, 1.0f + 1e-5f));
 }
 
 TEST(MathTest, SafeDivideReturnsNumeratorForSmallDenominator) {
-  EXPECT_NEAR(SafeDivide(5.0f, 2.0f), 2.5f, 1e-6f);
-  EXPECT_NEAR(SafeDivide(5.0f, 1e-8f), 5.0f, 1e-6f);
+  EXPECT_NEAR(SafeDivide(5.0f, 2.0f), 2.5f, kEpsilon);
+  EXPECT_NEAR(SafeDivide(5.0f, 1e-8f), 5.0f, kEpsilon);
+}
+
+TEST(MathTest, LogLinearIsLinearPlusConstantAtNEqualsOne) {
+  std::array<double, 5> test_values = {0.00001, 0.001, 0.1, 10, 1000};
+  double constant = LogLinear(test_values[0], 1.0) - test_values[0];
+  for (auto value : test_values) {
+    EXPECT_NEAR(LogLinear(value, 1.0), value + constant, kEpsilon);
+  }
+}
+
+TEST(MathTest, LogLinearIsLogAtNEqualsZero) {
+  std::array<double, 5> test_values = {0.00001, 0.001, 0.1, 10, 1000};
+  for (auto value : test_values) {
+    EXPECT_NEAR(LogLinear(value, 0.0), std::log(value), kEpsilon);
+  }
 }
 
 }  // namespace
