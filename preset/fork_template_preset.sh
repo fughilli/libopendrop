9a58cddfebf385a9adeba4ec195d99088e3c5155
Kevin Balke <fughilli@gmail.com>
2021-03-28 00:43:47 -0700

    Updated fork script to accept source preset name

--- preset/fork_template_preset.sh
+++ preset/fork_template_preset.sh
@@ -10,11 +10,14 @@ function echoerr() {
 function usage() {
   echoerr "USAGE:"
   echoerr ""
-  echoerr "$0 camel_case_name"
+  echoerr "$0 [-t template_name] camel_case_name"
   echoerr ""
   echoerr "Args:"
+  echoerr "  template_name:    Name of preset to fork, in camel_case (matches"
+  echoerr "                    regex \`^[a-z][0-9a-z_]*$\`). Defaults to "
+  echoerr "                    \"template_preset\"."
   echoerr "  camel_case_name:  Name of forked preset, in camel_case (matches"
-  echoerr "                    regex \`^[a-z][0-9a-z_]*$\`)"
+  echoerr "                    regex \`^[a-z][0-9a-z_]*$\`)."
 }
 
 function errexit() {
@@ -24,48 +27,76 @@ function errexit() {
 }
 
 function screaming_snake_case() {
-  name_snake_case=$1
+  local name_snake_case=$1
 
   echo "${name_snake_case}" | tr [:lower:] [:upper:]
 }
 
 function camel_case() {
-  name_snake_case=$1
+  local name_snake_case=$1
 
   echo "${name_snake_case}" | sed 's/\(^[a-z]\|_[a-z]\)/\U\1/g' | sed 's/_//g'
 }
 
 function recursive_replace() {
-  directory=$1
-  search_term=$2
-  replace_term=$3
+  local directory=$1
+  local search_term=$2
+  local replace_term=$3
 
   grep -rl "${search_term}" "${directory}" |
     xargs -I {} sed "s/${search_term}/${replace_term}/g" -i {}
 }
 
-name_snake_case=$1
+function check_snake_case() {
+  local name_snake_case=$1
+  if [[ -z $(echo "${name_snake_case}" | grep -e '^[a-z][0-9a-z_]*$') ]]; then
+    errexit "Input name ${name_snake_case} is not written in snake_case"
+  fi
+}
 
-if [[ $# != 1 ]]; then
-  errexit "$0 takes exactly 1 argument"
-fi
+template_name_snake_case="template_preset"
 
-if [[ -z $(echo "${name_snake_case}" | grep -e '^[a-z][0-9a-z_]*$') ]]; then
-  errexit "Input name ${name_snake_case} is not written in snake_case"
+while [[ ! -z "$@" ]]; do
+  arg=$1
+  shift
+
+  case $arg in
+    '-t')
+      template_name_snake_case=$1
+      shift
+      ;;
+
+    *)
+      if [[ ! -z "${name_snake_case}" ]]; then
+        errexit "Unknown argument: \"$1\""
+      fi
+      name_snake_case=$arg
+      ;;
+  esac
+done
+
+check_snake_case "${name_snake_case}"
+if [[ ! -z "${template_name_snake_case}" ]]; then
+  check_snake_case "${template_name_snake_case}"
 fi
 
 preset_dir="$(dirname $0)"
 name_camel_case=$(camel_case ${name_snake_case})
 name_screaming_snake_case=$(screaming_snake_case ${name_snake_case})
 
-cp -r "${preset_dir}/template_preset" "${preset_dir}/${name_snake_case}"
-recursive_replace "${preset_dir}/${name_snake_case}/" "TEMPLATE_PRESET" \
+template_name_camel_case=$(camel_case ${template_name_snake_case})
+template_name_screaming_snake_case=$(screaming_snake_case ${template_name_snake_case})
+
+echo "Forking \"${preset_dir}/${template_name_snake_case}\" ==> \"${preset_dir}/${name_snake_case}\"."
+
+cp -r "${preset_dir}/${template_name_snake_case}" "${preset_dir}/${name_snake_case}"
+recursive_replace "${preset_dir}/${name_snake_case}/" "${template_name_screaming_snake_case}" \
   "${name_screaming_snake_case}"
-recursive_replace "${preset_dir}/${name_snake_case}/" "template_preset" \
+recursive_replace "${preset_dir}/${name_snake_case}/" "${template_name_snake_case}" \
   "${name_snake_case}"
-recursive_replace "${preset_dir}/${name_snake_case}/" "TemplatePreset" \
+recursive_replace "${preset_dir}/${name_snake_case}/" "${template_name_camel_case}" \
   "${name_camel_case}"
-mv "${preset_dir}/${name_snake_case}/template_preset.cc" \
+mv "${preset_dir}/${name_snake_case}/${template_name_snake_case}.cc" \
   "${preset_dir}/${name_snake_case}/${name_snake_case}.cc"
-mv "${preset_dir}/${name_snake_case}/template_preset.h" \
+mv "${preset_dir}/${name_snake_case}/${template_name_snake_case}.h" \
   "${preset_dir}/${name_snake_case}/${name_snake_case}.h"
