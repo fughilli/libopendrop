44c5cc8971fe2ced3c2a479e933be805867f10f5
Kevin Balke <fughilli@gmail.com>
2021-10-22 03:14:54 -0700

    Adjust lifetime coefficients

--- main.cc
+++ main.cc
@@ -116,8 +116,8 @@ void NextPreset(OpenDropController *controller,
   }
   // TODO: Refactor such that preset geometry is configured after attaching to
   // the preset blender.
-  float duration = opendrop::Coefficients::Random<1>(15.0f, 30.0f)[0];
-  float ramp_duration = opendrop::Coefficients::Random<1>(1.0f, 10.0f)[0];
+  float duration = opendrop::Coefficients::Random<1>(20.0f, 100.0f)[0];
+  float ramp_duration = opendrop::Coefficients::Random<1>(1.0f, 5.0f)[0];
   auto status_or_render_target =
       gl::GlRenderTarget::MakeShared(0, 0, texture_manager);
   if (!status_or_render_target.ok()) {
