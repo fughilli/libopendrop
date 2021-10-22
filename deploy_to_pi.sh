af472a223adcf891640999474c623391a47334b7
Kevin Balke <fughilli@gmail.com>
2021-10-22 01:16:15 -0700

    Added argument for controlling target in deploy_to_pi.sh

--- deploy_to_pi.sh
+++ deploy_to_pi.sh
@@ -3,7 +3,24 @@
 set -o errexit
 set -o pipefail
 
+hostname=ledsuit
+
+while [[ ! -z "$@" ]]; do
+  arg=$1
+  shift
+
+  case $arg in
+    '-t')
+      hostname=$1
+      shift
+      ;;
+
+    *)
+      ;;
+  esac
+done
+
 bazelisk build --distinct_host_configuration --config=pi :main -c dbg
 cp ../bazel-bin/libopendrop/main /tmp/libopendrop
 chmod +rw /tmp/libopendrop
-scp /tmp/libopendrop pi@ledsuit:~
+scp /tmp/libopendrop "pi@${hostname}:~"
