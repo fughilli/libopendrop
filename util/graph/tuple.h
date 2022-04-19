39a5ba744ee56e6a93c392ae7d98a4c0be9bb51e
Kevin Balke <fughilli@gmail.com>
2022-04-18 23:59:16 -0700

    Implemented OpaqueTupleFactory and OpaqueTuple::Alias()

--- util/graph/tuple.h
+++ util/graph/tuple.h
@@ -6,8 +6,10 @@
 #include <memory>
 #include <type_traits>
 #include <utility>
+#include <vector>
 
 #include "absl/strings/str_format.h"
+#include "absl/types/span.h"
 #include "util/graph/types/types.h"
 #include "util/logging/logging.h"
 
@@ -44,14 +46,37 @@ struct TupleStorage {
 class OpaqueTuple {
  public:
   // Returns the list of types contained in this OpaqueTuple.
-  const std::vector<Type>& Types() const { return types; }
+  const std::vector<Type>& Types() const { return types_; }
 
   // Constructs an OpaqueTuple from the given set of types. Each type must be
   // default-constructible.
   template <typename... Ts>
-  static OpaqueTuple FromTypes() {
+  static OpaqueTuple ConstructFromTypes() {
     OpaqueTuple opaque_tuple{};
-    opaque_tuple.FromTypesImpl<Ts...>();
+    opaque_tuple.FromTypesImpl</*empty=*/false, Ts...>();
+    return opaque_tuple;
+  }
+
+  // Constructs an OpaqueTuple from the given set of types, leaving all cells
+  // empty (dangling pointers).
+  template <typename... Ts>
+  static OpaqueTuple EmptyFromTypes() {
+    OpaqueTuple opaque_tuple{};
+    opaque_tuple.FromTypesImpl</*empty=*/true, Ts...>();
+    return opaque_tuple;
+  }
+
+  // Constructs an OpaqueTuple from the given set of types and associated
+  // memory. The memory is assumed to be properly sized and aligned and have a
+  // correctly constructed object of the specified type within it.
+  static OpaqueTuple FromTypesAndMemory(
+      absl::Span<const std::pair<Type, std::shared_ptr<uint8_t>>>
+          types_and_memory) {
+    OpaqueTuple opaque_tuple{};
+    for (auto& [type, memory] : types_and_memory) {
+      opaque_tuple.types_.push_back(type);
+      opaque_tuple.cells_.push_back(Cell{.buffer = memory, .type = type});
+    }
     return opaque_tuple;
   }
 
@@ -59,15 +84,15 @@ class OpaqueTuple {
   // not match the stored value, or if the index is out of bounds, LOG(FATAL)s.
   template <typename T>
   T& Get(int index) {
-    if (index < -0 || index >= cells.size())
-      LOG(FATAL) << absl::StrFormat(
-          "index out of bounds (index = %d; cells.size() = %d)", index,
-          cells.size());
-    Cell& cell = cells[index];
+    CheckIndex(index);
+    Cell& cell = cells_[index];
     if (cell.type != ToType<T>())
       LOG(FATAL) << absl::StrFormat(
           "requesting incorrect type (T = %s) for cell %d of type %s",
           ToString(ToType<T>()), index, ToString(cell.type));
+    if (cell.buffer == nullptr)
+      LOG(FATAL) << absl::StrFormat("value in cell %d of type %s is nullptr",
+                                    index, ToString(cell.type));
 
     return *reinterpret_cast<T*>(cell.buffer.get());
   }
@@ -94,24 +119,61 @@ class OpaqueTuple {
     return ToRefTupleImpl<Ts...>(Indices{});
   }
 
+  // Reassigns the pointer in a cell of this OpaqueTuple to a cell in another
+  // OpaqueTuple (aliasing). LOG(FATAL)s if the cells are of incompatible types.
+  // Any value currently in the cell being aliased is destructed and the memory
+  // freed.
+  void Alias(int index, OpaqueTuple& target, int target_index) {
+    CheckIndex(index);
+    target.CheckIndex(target_index);
+
+    auto& cell = cells_[index];
+    auto& target_cell = target.cells_[target_index];
+    if (cell.type != target_cell.type)
+      LOG(FATAL) << absl::StrFormat(
+          "attempting to alias cell %d to target cell of incorrect type "
+          "(target cell %d type is %s, expected %s)",
+          index, target_index, ToString(target_cell.type), ToString(cell.type));
+    if (target_cell.buffer == nullptr)
+      LOG(FATAL) << absl::StrFormat(
+          "value in target cell %d of type %s is nullptr", target_index,
+          ToString(target_cell.type));
+
+    cell.buffer = target_cell.buffer;
+  }
+
  private:
+  OpaqueTuple() = default;
+
   // Storage for an element of OpaqueTuple.
   struct Cell {
     std::shared_ptr<uint8_t> buffer;
     Type type;
   };
 
-  // Implementation for FromTypes<Ts...>().
-  template <typename T1, typename T2, typename... Ts>
-  void FromTypesImpl() {
-    FromTypesImpl<T1>();
-    FromTypesImpl<T2, Ts...>();
+  // Checks whether or not an index is valid. LOG(FATAL)s if it is not.
+  void CheckIndex(int index) {
+    if (index < 0 || index >= cells_.size())
+      LOG(FATAL) << absl::StrFormat(
+          "index out of bounds (index = %d; cells_.size() = %d)", index,
+          cells_.size());
   }
-  template <typename T>
+
+  // Implementation for FromTypes<Ts...>().
+  template <bool empty, typename T1, typename T2, typename... Ts>
   void FromTypesImpl() {
-    cells.push_back(
-        Cell{.buffer = TupleStorage<T>::Allocate(), .type = ToType<T>()});
-    types.push_back(ToType<T>());
+    FromTypesImpl<empty, T1>();
+    FromTypesImpl<empty, T2, Ts...>();
+  }
+  template <bool empty, typename T>
+  void FromTypesImpl() {
+    if constexpr (empty) {
+      cells_.push_back(Cell{.buffer = nullptr, .type = ToType<T>()});
+    } else {
+      cells_.push_back(
+          Cell{.buffer = TupleStorage<T>::Allocate(), .type = ToType<T>()});
+    }
+    types_.push_back(ToType<T>());
   }
 
   // Implementation for AssignFrom<Ts...>().
@@ -133,15 +195,71 @@ class OpaqueTuple {
   }
 
   // Storage of all elements of OpaqueTuple.
-  std::vector<Cell> cells{};
+  std::vector<Cell> cells_{};
 
   // Convenience types list.
-  std::vector<Type> types{};
+  std::vector<Type> types_{};
+};
+
+class OpaqueTupleFactory {
+ public:
+  // Constructs an OpaqueTupleFactory which produces OpaqueTuple of the
+  // specified types.
+  template <typename... Ts>
+  static OpaqueTupleFactory FromTypes() {
+    OpaqueTupleFactory factory{};
+    factory.FromTypesImpl<Ts...>();
+    return factory;
+  }
+
+  // Constructs an OpaqueTuple of the types configured for this factory.
+  OpaqueTuple Construct() {
+    std::vector<std::pair<Type, std::shared_ptr<uint8_t>>> types_and_memory{};
+    for (int i = 0; i < types_.size(); ++i) {
+      types_and_memory.push_back(std::make_pair(types_[i], allocators_[i]()));
+    }
+
+    return OpaqueTuple::FromTypesAndMemory(types_and_memory);
+  }
+
+  // Produces an empty OpaqueTuple of the types configured for this factory.
+  OpaqueTuple Empty() {
+    std::vector<std::pair<Type, std::shared_ptr<uint8_t>>> types_and_memory{};
+    for (int i = 0; i < types_.size(); ++i) {
+      types_and_memory.push_back(std::make_pair(types_[i], nullptr));
+    }
+
+    return OpaqueTuple::FromTypesAndMemory(types_and_memory);
+  }
+
+ private:
+  OpaqueTupleFactory() = default;
+
+  // Implementation for FromTypes<Ts...>().
+  template <typename T1, typename T2, typename... Ts>
+  void FromTypesImpl() {
+    FromTypesImpl<T1>();
+    FromTypesImpl<T2, Ts...>();
+  }
+  template <typename T>
+  void FromTypesImpl() {
+    // TODO: Support memoizing constructor arguments.
+    //
+    // We work around this by only supporting objects in OpaqueTuple which are
+    // default-constructible.
+    allocators_.push_back(AllocatorType(TupleStorage<T>::template Allocate<>));
+    types_.push_back(ToType<T>());
+  }
+
+  using AllocatorType = std::function<std::shared_ptr<uint8_t>()>;
+
+  std::vector<Type> types_{};
+  std::vector<AllocatorType> allocators_{};
 };
 
 // Constructs a std::function() that:
-//   + Accepts opaque pointers to buffers of A and B (where A = InputTuple, B =
-//     OutputTuple).
+//   + Accepts `OpaqueTuple`s A and B (where A contains types InputTypes, and B
+//   is assignable from OutputTuple).
 //   + Computes `B = function(A)`.
 template <typename... InputTypes, typename OutputTuple>
 static std::function<void(OpaqueTuple&, OpaqueTuple&)> ToOpaqueFunction(
--- util/graph/tuple_test.cc
+++ util/graph/tuple_test.cc
@@ -14,14 +14,23 @@ namespace opendrop {
 namespace {
 
 TEST(TupleTest, CanConstructOpaqueTuple) {
-  auto opaque_tuple = OpaqueTuple::FromTypes<Unitary, Monotonic>();
+  auto opaque_tuple = OpaqueTuple::ConstructFromTypes<Unitary, Monotonic>();
 
   EXPECT_THAT(opaque_tuple.Types(),
               ::testing::ElementsAre(Type::kUnitary, Type::kMonotonic));
 }
 
+TEST(TupleTest, OpaqueTupleDiesOnAccessingEmpty) {
+  auto opaque_tuple = OpaqueTuple::EmptyFromTypes<Unitary, Monotonic>();
+
+  EXPECT_THAT(opaque_tuple.Types(),
+              ::testing::ElementsAre(Type::kUnitary, Type::kMonotonic));
+
+  EXPECT_DEATH({ opaque_tuple.Get<Unitary>(0) = 0.5f; }, "is nullptr");
+}
+
 TEST(TupleTest, CanStoreInOpaqueTuple) {
-  auto opaque_tuple = OpaqueTuple::FromTypes<Unitary, Monotonic>();
+  auto opaque_tuple = OpaqueTuple::ConstructFromTypes<Unitary, Monotonic>();
 
   opaque_tuple.Get<Unitary>(0) = 0.123f;
 
@@ -29,7 +38,7 @@ TEST(TupleTest, CanStoreInOpaqueTuple) {
 }
 
 TEST(TupleTest, OpaqueTupleDiesOnIncorrectGet) {
-  auto opaque_tuple = OpaqueTuple::FromTypes<Unitary, Monotonic>();
+  auto opaque_tuple = OpaqueTuple::ConstructFromTypes<Unitary, Monotonic>();
 
   EXPECT_DEATH({ opaque_tuple.Get<Monotonic>(0) = 10.0f; },
                "requesting incorrect type");
@@ -38,7 +47,8 @@ TEST(TupleTest, OpaqueTupleDiesOnIncorrectGet) {
 }
 
 TEST(TupleTest, OpaqueTupleCanBeAssignedFromStdTuple) {
-  auto opaque_tuple = OpaqueTuple::FromTypes<Unitary, Monotonic, Unitary>();
+  auto opaque_tuple =
+      OpaqueTuple::ConstructFromTypes<Unitary, Monotonic, Unitary>();
 
   auto tuple = std::tuple<Unitary, Monotonic, Unitary>(0.5f, 25.0f, 0.1f);
   opaque_tuple.AssignFrom(tuple);
@@ -48,9 +58,115 @@ TEST(TupleTest, OpaqueTupleCanBeAssignedFromStdTuple) {
   EXPECT_EQ(opaque_tuple.Get<Unitary>(2), 0.1f);
 }
 
+//
+// Tests covering aliasing.
+//
+
+TEST(TupleTest, OpaqueTupleCanAliasOtherTuple) {
+  auto opaque_tuple = OpaqueTuple::ConstructFromTypes<int, float>();
+  auto alias_opaque_tuple = OpaqueTuple::EmptyFromTypes<float, int>();
+
+  alias_opaque_tuple.Alias(0, opaque_tuple, 1);
+  alias_opaque_tuple.Alias(1, opaque_tuple, 0);
+
+  opaque_tuple.AssignFrom(std::make_tuple(123, 0.123f));
+
+  EXPECT_EQ(alias_opaque_tuple.Get<float>(0), 0.123f);
+  EXPECT_EQ(alias_opaque_tuple.Get<int>(1), 123);
+
+  // Aliasing is bidirectional.
+  alias_opaque_tuple.Get<float>(0) = 0.456f;
+  EXPECT_EQ(opaque_tuple.Get<float>(1), 0.456f);
+}
+
+TEST(TupleTest, OpaqueTupleDiesOnIncorrectAliasingTypes) {
+  auto opaque_tuple = OpaqueTuple::ConstructFromTypes<int, float>();
+  auto alias_opaque_tuple = OpaqueTuple::EmptyFromTypes<float, int>();
+
+  EXPECT_DEATH({ alias_opaque_tuple.Alias(0, opaque_tuple, 0); },
+               "incorrect type");
+}
+
+TEST(TupleTest, OpaqueTupleDiesOnOutOfBoundsIndex) {
+  auto opaque_tuple = OpaqueTuple::ConstructFromTypes<int, float>();
+  auto alias_opaque_tuple = OpaqueTuple::EmptyFromTypes<float, int>();
+
+  EXPECT_DEATH({ alias_opaque_tuple.Alias(2, opaque_tuple, 0); },
+               "out of bounds");
+  EXPECT_DEATH({ alias_opaque_tuple.Alias(0, opaque_tuple, -1); },
+               "out of bounds");
+}
+
+//
+// Tests covering constructor/destructor behavior.
+//
+
+constexpr int kDummyTypeValue = 999;
+
+enum ConstructionState {
+  kUnconstructed,
+  kConstructed,
+  kDestructed,
+};
+
+ConstructionState construction_state = ConstructionState::kUnconstructed;
+
+struct RaiiType {
+  constexpr static Type kType = static_cast<Type>(kDummyTypeValue);
+  RaiiType() { construction_state = ConstructionState::kConstructed; }
+  RaiiType(int x) {
+    construction_state = ConstructionState::kConstructed;
+    member_with_default = x;
+  }
+  ~RaiiType() { construction_state = ConstructionState::kDestructed; }
+
+  int member_with_default = 123;
+};
+
+TEST(TupleTest, OpaqueTupleInvokesConstructorAndDestructor) {
+  construction_state = ConstructionState::kUnconstructed;
+
+  ASSERT_EQ(construction_state, ConstructionState::kUnconstructed);
+  {
+    auto opaque_tuple = OpaqueTuple::ConstructFromTypes<RaiiType>();
+    EXPECT_EQ(construction_state, ConstructionState::kConstructed);
+    EXPECT_EQ(opaque_tuple.Get<RaiiType>(0).member_with_default, 123);
+  }
+  EXPECT_EQ(construction_state, ConstructionState::kDestructed);
+}
+
+//
+// Tests covering `OpaqueTupleFactory`.
+//
+
+TEST(TupleTest, OpaqueTupleFactoryConstructsTupleOfCorrectType) {
+  auto factory = OpaqueTupleFactory::FromTypes<int, float>();
+
+  OpaqueTuple tuple = factory.Construct();
+
+  EXPECT_THAT(tuple.Types(),
+              ::testing::ElementsAre(Type::kInteger, Type::kFloatGeneric));
+}
+
+TEST(TupleTest, OpaqueTupleFactoryConstructsDisjointTuples) {
+  auto factory = OpaqueTupleFactory::FromTypes<int, float>();
+
+  OpaqueTuple tuple_a = factory.Construct(), tuple_b = factory.Construct();
+
+  tuple_a.Get<int>(0) = 5;
+  tuple_b.Get<int>(0) = 3;
+
+  EXPECT_EQ(tuple_a.Get<int>(0), 5);
+  EXPECT_EQ(tuple_b.Get<int>(0), 3);
+}
+
+//
+// Tests covering ToOpaqueFunction().
+//
+
 TEST(TupleTest, OpaqueTupleFunctionInvocation) {
-  auto input_tuple = OpaqueTuple::FromTypes<float, float>();
-  auto output_tuple = OpaqueTuple::FromTypes<float, int>();
+  auto input_tuple = OpaqueTuple::ConstructFromTypes<float, float>();
+  auto output_tuple = OpaqueTuple::ConstructFromTypes<float, int>();
 
   std::function<std::tuple<float, int>(std::tuple<float&, float&>)> convert_fn =
       [](std::tuple<float&, float&> in) -> std::tuple<float, int> {
@@ -64,6 +180,99 @@ TEST(TupleTest, OpaqueTupleFunctionInvocation) {
 
   EXPECT_EQ(output_tuple.Get<float>(0), 13.5f);
   EXPECT_EQ(output_tuple.Get<int>(1), 35);
+}
+
+TEST(TupleTest, OpaqueTupleFactoryFunctionInvocation) {
+  auto input_tuple_factory = OpaqueTupleFactory::FromTypes<float, float>();
+  auto output_tuple_factory = OpaqueTupleFactory::FromTypes<float, int>();
+
+  std::function<std::tuple<float, int>(std::tuple<float&, float&>)> convert_fn =
+      [](std::tuple<float&, float&> in) -> std::tuple<float, int> {
+    auto& [a, b] = in;
+    return std::make_tuple(a + b, static_cast<int>(a * b));
+  };
+  auto opaque_fn = ToOpaqueFunction(convert_fn);
+
+  auto input_tuple = input_tuple_factory.Construct();
+  auto output_tuple = output_tuple_factory.Construct();
+
+  input_tuple.AssignFrom(std::make_tuple(10.0f, 3.5f));
+  opaque_fn(input_tuple, output_tuple);
+
+  EXPECT_EQ(output_tuple.Get<float>(0), 13.5f);
+  EXPECT_EQ(output_tuple.Get<int>(1), 35);
+}
+
+TEST(TupleTest, OpaqueTupleAliasingFunctionInvocations) {
+  // Compute a function graph:
+  //
+  // A := float(float, float)  // Add
+  // B := int(float, float)    // Multiply, convert to `int`
+  // C := float(int, float)    // Subtract
+  //
+  //        +---------------------------+
+  //        | input_tuple: float, float |
+  //        +---------------------------+
+  //            |                    |
+  //           [B]                  [C]
+  //            |                    |
+  //            V                    V
+  // +--------------------+  +------------------+
+  // | mid_tuple_1: float |  | mid_tuple_2: int |
+  // +--------------------+  +------------------+
+  //              |               |
+  //              |               |
+  //              V               V
+  //      +---------------------------------+
+  //      | mid_tuple_combined : int, float |
+  //      +---------------------------------+
+  //                      |
+  //                     [C]
+  //                      |
+  //                      V
+  //           +---------------------+
+  //           | output_tuple: float |
+  //           +---------------------+
+  //
+  // `mid_tuple_1` and `mid_tuple_2` own their storage.
+  //
+  // `mid_tuple_combined` is an aliasing OpaqueTuple over both of them.
+  auto input_tuple = OpaqueTuple::ConstructFromTypes<float, float>();
+  auto mid_tuple_1 = OpaqueTuple::ConstructFromTypes<float>();
+  auto mid_tuple_2 = OpaqueTuple::ConstructFromTypes<int>();
+  auto mid_tuple_combined = OpaqueTuple::EmptyFromTypes<int, float>();
+  auto output_tuple = OpaqueTuple::ConstructFromTypes<float>();
+
+  mid_tuple_combined.Alias(1, mid_tuple_1, 0);
+  mid_tuple_combined.Alias(0, mid_tuple_2, 0);
+
+  std::function<std::tuple<float>(std::tuple<float&, float&>)> a_fn =
+      [](std::tuple<float&, float&> in) -> std::tuple<float> {
+    auto& [a, b] = in;
+    return std::make_tuple(a + b);
+  };
+  std::function<std::tuple<int>(std::tuple<float&, float&>)> b_fn =
+      [](std::tuple<float&, float&> in) -> std::tuple<int> {
+    auto& [a, b] = in;
+    return std::make_tuple(static_cast<int>(a * b));
+  };
+  std::function<std::tuple<float>(std::tuple<int&, float&>)> c_fn =
+      [](std::tuple<int&, float&> in) -> std::tuple<float> {
+    auto& [a, b] = in;
+    return std::make_tuple(a - b);
+  };
+
+  auto opaque_a_fn = ToOpaqueFunction(a_fn);
+  auto opaque_b_fn = ToOpaqueFunction(b_fn);
+  auto opaque_c_fn = ToOpaqueFunction(c_fn);
+
+  input_tuple.AssignFrom(std::make_tuple(10.0f, 3.5f));
+
+  opaque_a_fn(input_tuple, mid_tuple_1);
+  opaque_b_fn(input_tuple, mid_tuple_2);
+  opaque_c_fn(mid_tuple_combined, output_tuple);
+
+  EXPECT_EQ(output_tuple.Get<float>(0), 21.5f);
 }
 
 }  // namespace
